bb0ff6acdbf0b57ff6ed719cb4885e603385fc22
Kyler <kpeek@hydoblaster.com>
2018-01-31 15:38:03 -0700

    Initial imports for PDF generator

---
+++ PdfGen.py
@@ -0,0 +1,15 @@
+from reportlab.pdfgen import canvas
+from reportlab.lob.pagesizes import letter
+from reportlab.platypus import Image, Paragraph
+from reportlab.rl_config import defaultPageSize
+from reportlab.lib.units import inch
+##For making ordered dictionaries
+import collections
+##For Image manipulation
+from PIL import ImageFont
+##For timestamp creation
+import time
+##For reading csv files
+import csv
+##For checking file existance withing system
+
